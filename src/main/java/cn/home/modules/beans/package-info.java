04b1eaccb0c239d02412c1c6b5cf6edded5d5bf4
doanldhan <shaoqinghanah@qq.com>
2017-12-05 09:35:52 +0800

    add some spring core container concept

--- src/main/java/cn/home/modules/beans/package-info.java
+++ src/main/java/cn/home/modules/beans/package-info.java
@@ -1,11 +1,37 @@
 /**
- * BeanFactory
- * ApplicationContext 
- * WebApplicationContext
- * ClassPathXmlApplicationContext 
- * FileSystemXmlApplicationContext
- * @author donald
- * 2017年12月4日
- * 下午10:19:14
+ * BeanFactory ApplicationContext WebApplicationContext
+ * ClassPathXmlApplicationContext FileSystemXmlApplicationContext BeanDefinition
+ * DefaultListableBeanFactory 
+ * =====================================================================
+ * InitializingBean DisposableBean
+ * 
+ * @PostConstruc
+ * @PreDestroy Methods annotated with @PostConstruct afterPropertiesSet() as
+ *             defined by the InitializingBean callback interface A custom
+ *             configured init() method
+ * 
+ *             Destroy methods are called in the same order:
+ * 
+ *             Methods annotated with @PreDestroy destroy() as defined by the
+ *             DisposableBean callback interface A custom configured destroy()
+ *             method SmartLifecycle LifecycleProcessor ApplicationContextAware
+ *             BeanFactoryAware ServletContextAware BeanFactoryPostProcessor
+ *             PropertyPlaceholderConfigurer FactoryBean
+ * ====================================================================
+ *             <context:annotation-config/>
+ * 
+ *             (The implicitly registered post-processors include
+ *             AutowiredAnnotationBeanPostProcessor,
+ *             CommonAnnotationBeanPostProcessor,
+ *             PersistenceAnnotationBeanPostProcessor, as well as the
+ *             aforementioned RequiredAnnotationBeanPostProcessor.) [Note]
+ * 
+ *             <context:annotation-config/> only looks for annotations on beans
+ *             in the same application context in which it is defined. This
+ *             means that, if you put <context:annotation-config/> in a
+ *             WebApplicationContext for a DispatcherServlet, it only checks
+ *             for @Autowired beans in your controllers, and not your services.
+ *            
+ * @author donald 2017年12月4日 下午10:19:14
  */
 package cn.home.modules.beans;
