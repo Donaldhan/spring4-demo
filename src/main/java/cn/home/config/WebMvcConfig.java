1115defc1b88dd4646d284f5886b410b32fe9c51
doanldhan <shaoqinghanah@qq.com>
2017-12-06 14:30:11 +0800

    init webmvc

--- src/main/java/cn/home/config/WebMvcConfig.java
+++ src/main/java/cn/home/config/WebMvcConfig.java
@@ -1,5 +1,12 @@
 package cn.home.config;
 
-public class WebMvcConfig {
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@Configuration
+@ComponentScan(basePackages = "cn.home.modules")
+@EnableWebMvc
+public class WebMvcConfig {
+	
 }
---
+++ src/main/java/cn/home/modules/webmvc/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * WebApplicationContext
+ * @author donald
+ * 2017年12月6日
+ * 下午12:59:19
+ */
+package cn.home.modules.webmvc;
