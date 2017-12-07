6f0eaa5c85dd4cc705f4eea58dd5e24ad52cdefe
doanldhan <shaoqinghanah@qq.com>
2017-12-07 22:27:10 +0800

    add bean annotation import

--- src/main/java/cn/home/config/WebSocketConfig.java
+++ src/main/java/cn/home/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package cn.home.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
